125321b0a55b068f85d1dd64ba1b9655d63a80de
Neta Fruchter <netafruchter@Netas-MBP.lan>
2024-11-30 12:10:23 +0200

    first try

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -1,3 +1,4 @@
+
 public class KeywordsDetector {
     public static void main(String[] args) {
         String[] sentences = {
@@ -21,6 +22,55 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for (int i = 0;i < sentences.length; i++) {
+            for (int j = 0;j < keywords.length; j++) {
+                //System.out.println(sentences[i]);
+                //System.out.println(keywords[j]);
+                if (contains(lowerCase(sentences[i]),(lowerCase(keywords[j])))) {
+                    System.out.println(sentences[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static String lowerCase(String str) {
+        String lowerStr = "";
+        
+        for (int i = 0; i < str.length(); i++) {
+            //if the char IS A-Z, add the diff to a-z. else, just add the char.
+            lowerStr += str.charAt(i) >= 65 && str.charAt(i) <= 90 ? (char)(str.charAt(i) + 32) : str.charAt(i);
+        }
+        return lowerStr;
+    }
+
+     /** If str1 contains str2, returns true; otherwise returns false. */
+    public static boolean contains(String str1, String str2) {
+        if (str1.length() < str2.length() || (str1.length() == str2.length() && !(str1 == str2))) {
+            return false;
+        }
+        if (str1 == str2){
+            return true;
+        }
+
+        int counter = 0;
+        for (int i = 0; i < str1.length(); i ++) {
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i+j) != str2.charAt(j)) {  
+                    counter = 0;
+                    break;
+                } 
+                else {
+                    if (str2.length() > str1.substring(i).length()){
+                        return false;
+                    }
+                    counter++;
+                }
+            }
+            if (counter == str2.length()) {
+                return true;
+            }
+        }   
+        return false;
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,29 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean arr[] = new boolean[n];
+        
+        arr[0] = false; arr[1] = false;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = true;
+        }
+
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++){
+                if (j%i == 0 && j != i){
+                    arr[j] = false;
+                }
+            }
+        }
+
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i]) {
+                counter++;
+                System.out.println(i);
+            }
+        }
+        System.out.println("There are " + counter +" primes between 2 and " + n + " (" + (int)((counter/(double)(n))*100) +"% are primes)");
+
     }
 }
